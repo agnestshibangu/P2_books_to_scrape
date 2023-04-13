85a28438c1bed6750ce91c5da7aa90a2fb4515f6
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2023-04-13 19:11:49 +0200

    etape 3

--- etape3.py
+++ etape3.py
@@ -5,30 +5,54 @@ import pandas
 
 #### Étape 3 : Extraire toutes les données des produits d’une catégorie ####
 
-url = 'http://books.toscrape.com/'
+url = 'https://books.toscrape.com/catalogue/category/books/womens-fiction_9/index.html'
 
 response = requests.get(url)
+
+headersArray = numpy.array([])
+rowsArray = numpy.array([])
+
+# if response.ok:
+#     links = []
+#     datapage = BeautifulSoup(response.text, 'lxml')
+#     singlebooklinks = datapage.find_all('h3')
+#     # print(singlebooklinks)
+#     for singlebooklink in singlebooklinks:
+#         a = singlebooklink.find('a')
+#         link = a['href']
+#         links.append('http://books.toscrape.com/' + link)
+# print(len(links))
+
+# with open('urls.csv', 'w') as file:
+#     for link in links:
+#         file.write(link + '\n')
 
 if response.ok:
     links = []
     datapage = BeautifulSoup(response.text, 'lxml')
-    sidedivcat = datapage.find('div', {'class':'side_categories'})
-    dbs =  sidedivcat.find_all('li')
-    for db in dbs:
-        a = db.find('a')
+    singlebooklinks = datapage.find_all('h3')
+    # print(singlebooklinks)
+    for singlebooklink in singlebooklinks:
+        a = singlebooklink.find('a')
         link = a['href']
-        links.append(link)
-        print(links)
-        ####
         links.append('http://books.toscrape.com/' + link)
+    for link in links:
+        url = link.strip()
+        response = requests.get(url)
+        print(response)
+    #         rows = data.find_all('td')
+    #         for row in rows:
+    #             rowsArray = numpy.append(rowsArray, row.text)
+     
 
-    print(links)
 
-
+            # for header in headers:
+            #     headersArray = numpy.append(headersArray, header.text)
+            # datas = data.find_all('td')
+            # for row in rows:
+            # rowsArray = numpy.append(rowsArray, row.text)
 
 
         
-
-    
 
 
